8cccffee56d523b8320a084405bab39b6f29db2f
Silvia Desquiavoni <silviabortolote@gmail.com>
2021-10-23 22:28:50 +0200

    feat: initt project

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {}
+
+
+}
